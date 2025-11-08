c54e44f419b5b05f1c605491ee58ae75108e9703
Ethan Ooi <ethanooi@Ethans-MacBook-Pro.local>
2025-11-08 02:06:03 -0500

    11-8-25

--- scripts/prep_data.py
+++ scripts/prep_data.py
@@ -91,3 +91,51 @@ edges = (
     .select('pid', 'tid')
 )
 edges.write.mode('overwrite').parquet('parquet_data/edges')
+
+edges_shift = edges.withColumn("tid", F.col("tid") + 1)
+track_features_shift = track_features.withColumn("tid", F.col("tid") + 1)
+
+# Building the length = 20, arrays for the tids, and create a masking object
+PAD_ID = 0
+max_length = 20
+
+
+# For each playlist, create an array of all track ids in that playlist
+pl_seqs = (edges_shift
+         .groupby('pid')
+         .agg(F.array_distinct(F.collect_list('tid')).alias('tids'))
+         .filter(F.size('tids') >= 2)
+)
+
+pairs = (
+    pl_seqs
+    # 1) Pick a positive by shuffling the tids per playlist, then take the first
+    .withColumn('shuffle', F.shuffle(F.col('tids')))
+    .withColumn('pos_tid', F.element_at(F.col('shuffle'), 1))
+    # 2) Remove the positive from the context pool
+    .withColumn('remain', F.filter('tids', lambda x: x != F.col('pos_tid')))
+    # 3) Take up the max_length random items, shuffle then slice
+    .withColumn('items', F.slice(F.shuffle(F.col('remain')), 1, max_length))
+    # 4) Build mask & padding to max_length
+    .withColumn('len', F.size('items'))
+    .withColumn('pad_len', F.greatest(F.lit(0), F.lit(max_length) - F.col('len')))
+    .withColumn('tokens', F.concat(F.col('items'), F.array_repeat(F.lit(PAD_ID), F.col('pad_len'))))
+    .withColumn('mask', F.concat(F.array_repeat(F.lit(1), F.col('len')), F.array_repeat(F.lit(0), F.col('pad_len'))))
+    # 5) Select relevant items
+    .select('pid', 'tokens', 'mask', 'pos_tid')
+)
+pairs.show(15, truncate=False)
+
+# Creates a random 95/5 train/validation split
+bucketed = pairs.withColumn('bucket', F.pmod(F.abs(F.hash('pid')), F.lit(100))) # Creates a random positive integer & modulus divides by 100. Essentially randomly groups each pid into 100 buckets
+train_pairs = bucketed.filter('bucket < 95').select('pid', 'tokens', 'mask', 'pos_tid') # ~95% of the data
+val_pairs = bucketed.filter('bucket >= 95').select('pid', 'tokens', 'mask', 'pos_tid') # ~5% of the data
+
+print("train rows:", train_pairs.count())
+print("val rows:",   val_pairs.count())
+
+n_tracks = edges_shift.agg(F.max('tid').alias('max_tid')).collect()[0]['max_tid']
+print('n_tracks (embedding size):', int(n_tracks) + 1)
+
+train_pairs.write.mode('overwrite').parquet('parquet_data/train_pairs')
+val_pairs.write.mode('overwrite').parquet('parquet_data/val_pairs')
