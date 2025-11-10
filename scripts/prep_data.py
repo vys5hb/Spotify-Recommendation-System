53298d3a6c806609417a173d7721f68ebd6a3d33
Ethan Ooi <ethanooi@Ethans-MacBook-Pro.local>
2025-11-10 00:37:29 -0500

    11-10-25

--- scripts/prep_data.py
+++ scripts/prep_data.py
@@ -25,64 +25,14 @@ playlist_tracks = spark.read.parquet("parquet_data/playlist_tracks")
 playlists = spark.read.parquet("parquet_data/playlists")
 tracks = spark.read.parquet("parquet_data/tracks")
 
-# Creates a unique tid (int) for each track_uri (string).
-# Item vocabulary - Track
+# Create a unique track id, artist id, and album id
 track_id = (
-    tracks.select("track_uri")
+    tracks.select('track_uri', 'artist_uri', 'album_uri')
     .dropDuplicates()
-    .withColumn("tid", F.dense_rank().over(W.orderBy("track_uri")) - 1)
+    .withColumn("tid", F.dense_rank().over(W.orderBy("track_uri")))
+    .withColumn('aid', F.dense_rank().over(W.orderBy('artist_uri')))
+    .withColumn('alid', F.dense_rank().over(W.orderBy('album_uri')))
 )
-# Calculate # of playlists each track appears in
-track_cnt = (
-    playlist_tracks.join(track_id, on='track_uri', how='inner')
-    .groupBy('tid')
-    .agg(F.countDistinct('pid').alias('track_cnt'))
-)
-# Saves the track metadata for later training and inference.
-# Item features - Track
-track_features = (
-    tracks.join(track_id, on="track_uri", how="inner")
-    .join(track_cnt, on='tid', how='left')
-    .select('tid', 'artist_uri', 'artist_name', 'album_uri', 'album_name', 'track_duration', 'track_cnt')
-)
-# Write track_features to parquet for model training
-track_features.write.mode('overwrite').parquet('parquet_data/track_features')
-
-# Creates a unique aid (int) for each artist_uri (string).
-# Item vocabulary - Artist
-artist_id = (
-    tracks.select('artist_uri', 'artist_name')
-    .dropDuplicates(['artist_uri'])
-    .withColumn("aid", F.dense_rank().over(W.orderBy("artist_uri")) - 1)
-)
-# Calculate # of playlists each artist appears in, and total # of appearances for each artist
-artist_cnt = (
-    playlist_tracks.alias('pt')
-    .join(tracks.select('track_uri', 'artist_uri'), on='track_uri', how='inner')
-    .groupBy('artist_uri')
-    .agg(F.countDistinct('pt.pid').alias('artist_playlist_cnt'),
-         F.count('pt.pid').alias('artist_cnt')
-    )
-)
-# Item features - Artist
-artist_features = (
-    artist_id.join(artist_cnt, on='artist_uri', how='left')
-    .select('aid', 'artist_uri', 'artist_name', 'artist_playlist_cnt', 'artist_cnt')
-)
-# Write artist_features to parquet for model training
-artist_features.write.mode('overwrite').parquet('parquet_data/artist_features')
-
-# Saves the playlist metadata for later training and inference.
-# User Features
-latest_edit = playlists.agg(F.max("modified_at").alias("now")).collect()[0]["now"]
-playlist_features = (
-    playlists.select('pid', 'name', 'num_tracks', 'num_artists', 'num_albums', 'playlist_duration', 'modified_at', 'collaborative')
-    .withColumn("days_since_modified", (F.lit(latest_edit) - F.col("modified_at")) / 86400)
-    .drop("modified_at")
-    .select('pid', 'name', 'num_tracks', 'num_artists', 'num_albums', 'playlist_duration', 'days_since_modified', 'collaborative')
-)
-# Write playlist_features to parquet for model training
-playlist_features.write.mode('overwrite').parquet('parquet_data/playlist_features')
 
 # Joins unique tid(int) to each pid(int) from playlist_tracks for collaborative filtering.
 # Edges are the interaction matrix
@@ -91,22 +41,18 @@ edges = (
     .select('pid', 'tid')
 )
 edges.write.mode('overwrite').parquet('parquet_data/edges')
-
-edges_shift = edges.withColumn("tid", F.col("tid") + 1)
-track_features_shift = track_features.withColumn("tid", F.col("tid") + 1)
-
+print('Successfully saved edges.')
 # Building the length = 20, arrays for the tids, and create a masking object
 PAD_ID = 0
 max_length = 20
 
-
 # For each playlist, create an array of all track ids in that playlist
-pl_seqs = (edges_shift
+pl_seqs = (edges
          .groupby('pid')
          .agg(F.array_distinct(F.collect_list('tid')).alias('tids'))
          .filter(F.size('tids') >= 2)
 )
-
+# Creates unique tokens per token id, and a masking array for filler tokens. Randomly selects a positive token id to train model on positive associations
 pairs = (
     pl_seqs
     # 1) Pick a positive by shuffling the tids per playlist, then take the first
@@ -124,18 +70,73 @@ pairs = (
     # 5) Select relevant items
     .select('pid', 'tokens', 'mask', 'pos_tid')
 )
-pairs.show(15, truncate=False)
 
 # Creates a random 95/5 train/validation split
 bucketed = pairs.withColumn('bucket', F.pmod(F.abs(F.hash('pid')), F.lit(100))) # Creates a random positive integer & modulus divides by 100. Essentially randomly groups each pid into 100 buckets
 train_pairs = bucketed.filter('bucket < 95').select('pid', 'tokens', 'mask', 'pos_tid') # ~95% of the data
 val_pairs = bucketed.filter('bucket >= 95').select('pid', 'tokens', 'mask', 'pos_tid') # ~5% of the data
 
-print("train rows:", train_pairs.count())
-print("val rows:",   val_pairs.count())
-
-n_tracks = edges_shift.agg(F.max('tid').alias('max_tid')).collect()[0]['max_tid']
-print('n_tracks (embedding size):', int(n_tracks) + 1)
-
+# Write train_pairs to parquet for model training
 train_pairs.write.mode('overwrite').parquet('parquet_data/train_pairs')
+print('Successfully saved train_pairs.')
+# Write val_pairs to parquet for model training
 val_pairs.write.mode('overwrite').parquet('parquet_data/val_pairs')
+print('Successfully saved val_pairs.')
+
+# Select only the playlist ids from the train data split
+train_edges = edges.join(train_pairs, on='pid', how='inner')
+
+# Calculate # of playlists each track appears in only from the train data split
+train_track_cnt = train_edges.groupBy('tid').agg(F.countDistinct('pid').alias('track_cnt'))
+
+# Calculate # of playlists each artist appears in only from the train data split
+train_artist_cnt = (
+    train_edges.join(track_id.select('tid', 'artist_uri'), on='tid', how='inner')
+    .groupBy('artist_uri')
+    .agg(F.countDistinct('pid').alias('artist_cnt'))
+)
+# Saves the track metadata for later training and inference.
+# Item features
+track_features = (
+    track_id
+    .join(train_track_cnt, on="tid", how="left")
+    .join(train_artist_cnt, on='artist_uri', how='left')
+    .select('tid', 'aid', 'alid', 'track_cnt', 'artist_cnt')
+    .fillna({'track_cnt': 0, 'artist_cnt': 0})
+)
+track_features = (
+    track_features
+    .withColumn('log_track_cnt', F.log1p('track_cnt'))
+    .withColumn('log_artist_cnt', F.log1p('artist_cnt'))
+)
+z_score = track_features.agg(
+    F.mean('log_track_cnt').alias('mean_track_cnt'),
+    F.stddev_pop('log_track_cnt').alias('std_track_cnt'),
+    F.mean('log_artist_cnt').alias('mean_artist_cnt'),
+    F.stddev_pop('log_artist_cnt').alias('std_artist_cnt')
+).collect()[0]
+track_features = (
+    track_features
+    .withColumn('z_log_track_cnt', (F.col('log_track_cnt') - F.lit(z_score['mean_track_cnt'])) / F.lit(z_score['std_track_cnt']))
+    .withColumn('z_log_artist_cnt', (F.col('log_artist_cnt') - F.lit(z_score['mean_artist_cnt'])) / F.lit(z_score['std_artist_cnt']))
+    .select('tid', 'aid', 'alid', 'z_log_track_cnt', 'z_log_artist_cnt')
+)
+track_features.write.mode('overwrite').parquet('parquet_data/track_features')
+print('Successfully saved track_features.')
+
+# Saves the playlist metadata for later training and inference.
+# User Features
+latest_edit = playlists.agg(F.max("modified_at").alias("now")).collect()[0]["now"]
+playlist_features = (
+    playlists.select('pid', 'name', 'num_tracks', 'num_artists', 'num_albums', 'playlist_duration', 'modified_at', 'collaborative')
+    .withColumn("days_since_modified", (F.lit(latest_edit) - F.col("modified_at")) / 86400)
+    .withColumn('log_n_tracks', F.log1p('num_tracks'))
+    .withColumn('log_n_artists', F.log1p('num_artists'))
+    .withColumn('log_n_albums', F.log1p('num_albums'))
+    .withColumn('log_pl_duration', F.log1p('playlist_duration'))
+    .withColumn('log_days_mod', F.log1p('days_since_modified'))
+    .select('pid', 'name', 'log_n_tracks', 'log_n_artists', 'log_n_albums', 'log_pl_duration', 'log_days_mod', 'collaborative')
+)
+# Write playlist_features to parquet for model training
+playlist_features.write.mode('overwrite').parquet('parquet_data/playlist_features')
+print('Successfully saved playlist_features.')
